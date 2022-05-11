0c8d094214e0addc6e48db1488ebc40608ba0833
GeorgeLee <14094116@student.uts.edu.au>
2022-05-11 19:01:07 +1000

    Initial Commit

---
+++ cinemasBooking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinemasBooking
+//
+//  Created by George Lee on 11/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
